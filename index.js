8046756a0113a93d2617e1d03ae526e686110cf5
thainan_10@hotmail.com <thainan10@gmail.com>
2017-01-03 20:09:04 -0300

    Initialize the npm project.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 const pg = require('pg');
-const conString = 'postgres://thainan:n37d5rqz@localhost/node_hero';
+const conString = 'postgres://Thainan:n37d5rqz@localhost/node_hero';
 
 app.listen(port, (err) => {
   if (err) {
@@ -15,24 +15,9 @@ app.listen(port, (err) => {
   console.log(`server is listening on ${port}`);
 });
 
-// pg.connect(conString, function (err, client, done) {
-//   if (err) {
-//     return console.error('error fetching client from pool', err)
-//   };
-//   client.query('SELECT $1::varchar AS my_first_query', ['node hero'], function (err, result) {
-//     done();
-//
-//     if (err) {
-//       return console.error('error happened during query', err);
-//     };
-//     console.log(result.rows[0]);
-//     process.exit(0);
-//   });
-// });
-
 app.post('/users', function(req, res, next) {
   const user = req.body;
-  console.log('teste: ', user);
+  console.log('teste: ', req);
 
   pg.connect(conString, function (err, client, done) {
     if (err) {
